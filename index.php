aa4bf2efea87ce24a3469098abc9fb61fc206540
Julia Hendrickson <julmicbud@gmail.com>
2016-08-26 15:50:50 -0400

    looping through posts

--- index.php
+++ index.php
@@ -15,11 +15,20 @@
 </header>	
 	<div class="main">
 		<?php if(have_posts()) : ?>
+			<?php while(have_posts()): the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+				<div class="meta">
+					Created By <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
+				</div>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
 		<?php else : ?>
-			<?php echo wpautop(); ?>
+			<?php echo wpautop('Sorry, No posts were found'); ?>
 		<?php endif; ?>
-		
-	</div>
+		</div>
+		<footer>
+			<p>&copy; <?php the_date('Y'); ?></p>
+		</footer>
 </body>
 
 </html>
