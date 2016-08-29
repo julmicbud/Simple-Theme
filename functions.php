61528ca9f4ae4afb30de2a01adee933ef559a3f1
Julia Hendrickson <julmicbud@gmail.com>
2016-08-29 15:08:06 -0400

    pages and menus

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 functions simple_theme_setup(){
 	//featured image support
 	add_theme_support('post-thumnails');
+	
+	// Menus
+	register_nav_menus(array(
+		'primary' =>__('Primary Menu')
+		));
 }
 
 add_action('after_setup_theme', 'simple_theme_setup');
+
+//Excerpt Length
+function set_excerpt_length(){
+	return 60;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
+
